57c0409d94675e5eb667c3ecda3ce7e41c73bd56
NoahAwad <NoahAwad@wifi-roaming-128-4-201-85.host.udel.edu>
2016-03-22 02:48:01 -0400

    initial commit

---
+++ Midterm Coding Project/src/main/java/com/cisc181/core/Student.java
@@ -0,0 +1,47 @@
+package com.cisc181.core;
+
+import java.util.Date;
+import java.util.UUID;
+
+import com.cisc181.eNums.eMajor;
+
+public class Student extends Person {
+	private eMajor Major;
+	
+	public eMajor getMajor() {
+		return Major;
+	}
+	public void setMajor(eMajor major) {
+		Major = major;
+	}
+	public void setStudentID(UUID studentID) {
+		StudentID = studentID;
+	}
+
+	private UUID StudentID;
+	
+	
+    
+    public UUID getStudentID(){
+    	return this.StudentID;
+    }
+    
+	public Student(String FirstName, String MiddleName, String LastName,Date DOB, eMajor Major,
+			String Address, String Phone_number, String Email) throws PersonException
+	{
+		super(FirstName, MiddleName, LastName, DOB, Address, Phone_number, Email);
+		this.StudentID = UUID.randomUUID();
+		this.Major = Major;
+		
+	}
+	
+	@Override
+	public void PrintName() {
+		System.out.println(getLastName() + ","  + getFirstName() + ' ' + getMiddleName());
+	}
+
+	public void PrintName(boolean bnormal)
+	{
+		super.PrintName();
+	}
+}
